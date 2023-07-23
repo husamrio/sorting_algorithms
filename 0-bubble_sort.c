ffcec36f45b02fe219999dd8054dc3f360b7634f
husamrio <zulfiqrd@gmail.com>
2023-07-23 14:17:08 +0300

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+
+
+/**
+* swap_ints - Switch two array integers.
+* @b: Second integer to switch.
+* @a: First integer to switch.
+*/
+void swap_ints(int *a, int *b)
+{
+int tmp;
+
+tmp = *a;
+*a = *b;
+*b = tmp;
+}
+
+/**
+* bubble_sort - Arrange array integers in ascending order.
+* @array: Integer array to arrange.
+* @size: Array size.
+*
+* Description: Show array after each switch.
+*/
+void bubble_sort(int *array, size_t size)
+{
+size_t i, len = size;
+bool bubbly = false;
+
+if (array == NULL || size < 2)
+return;
+
+while (bubbly == false)
+{
+bubbly = true;
+for (i = 0; i < len - 1; i++)
+{
+if (array[i] > array[i + 1])
+{
+swap_ints(array + i, array + i + 1);
+print_array(array, size);
+bubbly = false;
+}
+}
+len--;
+}
+}
---
+++ sort.h
@@ -0,0 +1,57 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - Node of a doubly-linked list
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Bitonic sort comparison direction macros */
+#define DOWN 1
+#define UP 0
+
+/**
+ * enum bool - Boolean Enumeration values.
+ * @true: Equals 1.
+ * @false: Equals 0.
+ 
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+
+
+/* Helper printing functions */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/* Sort algorithms */
+void quick_sort_hoare(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
+#endif /* SORT_H */
